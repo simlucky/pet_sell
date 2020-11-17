88c19ebef3642c991e04fe880d91a78284c5c63f
xiongwendeng <xiongwendeng@caih.com>
2020-11-17 17:20:51 +0800

    基础架构搭建

---
+++ src/pages/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <view>
+        <view class="login-wrap">
+            <u-input v-model="value" :type="type" :border="border" />
+            <u-input v-model="value" :type="type" :border="border" />
+        </view>
+    </view>
+</template>
+
+<script>
+    export default{
+        data(){
+
+        }
+    }
+</script>
+
+<style>
+    
+</style>
