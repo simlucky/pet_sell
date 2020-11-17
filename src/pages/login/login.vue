c6303f98336a9dae4a71b5885fea8b401b5b5bdb
xiongwendeng <xiongwendeng@caih.com>
2020-11-17 18:00:42 +0800

    增加登陆页

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,20 +1,41 @@
 <template>
-    <view>
+    <view class="wrap">
         <view class="login-wrap">
-            <u-input v-model="value" :type="type" :border="border" />
-            <u-input v-model="value" :type="type" :border="border" />
+            <u-form :model="loginForm" ref="uForm">
+                <u-form-item label="账号">
+                    <u-input v-model="loginForm.password" placeholder="请输入账号" />
+                </u-form-item>
+                <u-form-item label="密码">
+                    <u-input v-model="loginForm.username" placeholder="请输入密码" />
+                </u-form-item>
+                <view class="register-hint">尚未注册？点击注册</view>
+                <u-button type="primary">登陆</u-button>
+            </u-form>
         </view>
     </view>
 </template>
 
 <script>
-    export default{
-        data(){
-
+    export default {
+        data() {
+            return {
+                loginForm: {
+                    password: null,
+                    username: null,
+                    role: null
+                }
+            }
         }
     }
 </script>
 
-<style>
-    
+<style lang='scss' scoped>
+.wrap{
+    padding: 20rpx;
+    .register-hint{
+        text-align: center;
+        color: #999;
+    }
+}
+
 </style>
